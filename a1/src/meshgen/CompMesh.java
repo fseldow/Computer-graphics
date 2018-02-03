1b73db18fae3f8613a29af36908237013e715673
fseldow <lixinhe126@126.com>
2018-02-02 23:06:56 -0500

    result files adjustment

--- a1/src/meshgen/CompMesh.java
+++ a1/src/meshgen/CompMesh.java
@@ -8,8 +8,8 @@ public class CompMesh {
 
 	public static void main(String[] args) throws OBJFileFormatException, IOException {
 		// TODO Auto-generated method stub
-		OBJMesh m1=new OBJMesh("haha4.obj");
-		OBJMesh m2=new OBJMesh("data/referencetorus.obj");
+		OBJMesh m1=new OBJMesh("horse-norms.obj");
+		OBJMesh m2=new OBJMesh("data/horse-norms-reference.obj");
 		float ep=(float)0.00001;
 		System.out.print(OBJMesh.compare(m1,m2,true,ep));
 	}
